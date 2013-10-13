23fa93a496b05c4f1f54b59be87c69c572389e03
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-10-13 20:47:01 +0200

    solve_all script

---
+++ production/solve_all.sh
@@ -0,0 +1,4 @@
+pypy solver.py 10.4 7000 >../data/mun.txt &
+pypy solver.py 1.5 12000 >../data/kerbol.txt &
+pypy solver.py 11 10500 >../data/moho.txt &
+pypy solver.py 12 14800 >../data/laythe.txt &
